8a191d5742bfacf88698d190d601e5dd05e25396
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-11-27 19:06:43 +0900

    remove T

--- src/macros.rs
+++ src/macros.rs
@@ -33,23 +33,6 @@ macro_rules! range_opt {
 /// 厳密な拡張記法に対応
 #[macro_export]
 macro_rules! id {
-    // 時空間が指定されているパターン
-    (
-        z: $z:expr,
-        f: $f:tt,
-        x: $x:tt,
-        y: $y:tt,
-        i: $i:expr,
-        t: $t:tt $(,)?
-    ) => {{
-        let f_range: [Option<i64>; 2] = $crate::range_opt!($f);
-        let x_range: [Option<u64>; 2] = $crate::range_opt!($x);
-        let y_range: [Option<u64>; 2] = $crate::range_opt!($y);
-        let t_range: [Option<u64>; 2] = $crate::range_opt!($t);
-
-        SpaceTimeID::new($z, f_range, x_range, y_range, $i, t_range)
-    }};
-
     // 時間情報が省略されたパターン
     (
         z: $z:expr,
@@ -61,6 +44,6 @@ macro_rules! id {
         let x_range: [Option<u64>; 2] = $crate::range_opt!($x);
         let y_range: [Option<u64>; 2] = $crate::range_opt!($y);
 
-        SpaceTimeID::new($z, f_range, x_range, y_range, 0, [None, None])
+        SpaceTimeID::new($z, f_range, x_range, y_range)
     }};
 }
--- src/space_time_id/mod.rs
+++ src/space_time_id/mod.rs
@@ -21,8 +21,8 @@ pub struct SpaceTimeID {
     pub f: [i64; 2],
     pub x: [u64; 2],
     pub y: [u64; 2],
-    pub i: u64,
-    pub t: [u64; 2],
+    // pub i: u64,
+    // pub t: [u64; 2],
 }
 
 impl SpaceTimeID {
@@ -31,8 +31,8 @@ impl SpaceTimeID {
         f: [Option<i64>; 2],
         x: [Option<u64>; 2],
         y: [Option<u64>; 2],
-        i: u64,
-        t: [Option<u64>; 2],
+        // i: u64,
+        // t: [Option<u64>; 2],
     ) -> Result<Self, Error> {
         if z > MAX_ZOOM_LEVEL as u8 {
             return Err(Error::ZoomLevelOutOfRange { zoom_level: z });
@@ -47,29 +47,29 @@ impl SpaceTimeID {
         let new_x = normalize_dimension(x, 0, xy_max, valid_range_x, z)?;
         let new_y = normalize_dimension(y, 0, xy_max, valid_range_y, z)?;
 
-        // 時間軸も Option を展開
-        let start_t = t[0]
-            .unwrap_or(0)
-            .checked_mul(i)
-            .ok_or_else(|| Error::TimeOverflow {
-                t: t[0].unwrap_or(0),
-                i,
-            })?;
-        let end_t = t[1]
-            .unwrap_or(u64::MAX)
-            .checked_mul(i)
-            .ok_or_else(|| Error::TimeOverflow {
-                t: t[1].unwrap_or(u64::MAX),
-                i,
-            })?;
+        // // 時間軸も Option を展開
+        // let start_t = t[0]
+        //     .unwrap_or(0)
+        //     .checked_mul(i)
+        //     .ok_or_else(|| Error::TimeOverflow {
+        //         t: t[0].unwrap_or(0),
+        //         i,
+        //     })?;
+        // let end_t = t[1]
+        //     .unwrap_or(u64::MAX)
+        //     .checked_mul(i)
+        //     .ok_or_else(|| Error::TimeOverflow {
+        //         t: t[1].unwrap_or(u64::MAX),
+        //         i,
+        //     })?;
 
         Ok(SpaceTimeID {
             z,
             f: new_f,
             x: new_x,
             y: new_y,
-            i,
-            t: [start_t, end_t],
+            // i,
+            // t: [start_t, end_t],
         })
     }
 }
