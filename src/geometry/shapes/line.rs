2ab2b3cb70069667cf55013574795c087a70d218
knyg2 <knyg2313@gmail.com>
2025-12-29 17:31:03 +0900

    やり直し2

--- src/geometry/shapes/line.rs
+++ src/geometry/shapes/line.rs
@@ -1,9 +1,9 @@
-use core::f64;
+use std::collections::HashSet;
 
 use crate::{
     error::Error,
     geometry::{constants::WGS84_A, coordinate::Coordinate, ecef::Ecef},
-    id::space_id::{self, constants::MAX_ZOOM_LEVEL, single::SingleID},
+    id::space_id::{constants::MAX_ZOOM_LEVEL, single::SingleID},
 };
 pub fn line(z: u8, a: Coordinate, b: Coordinate) -> Result<impl Iterator<Item = SingleID>, Error> {
     if z > MAX_ZOOM_LEVEL as u8 {
