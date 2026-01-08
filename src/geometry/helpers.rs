41c5aedaa4b9110835579481c22a03fad199f3ed
knyg2 <knyg2313@gmail.com>
2026-01-09 01:08:26 +0900

    新関数作成

--- src/geometry/helpers.rs
+++ src/geometry/helpers.rs
@@ -1,5 +1,5 @@
 use crate::{
-    geometry::ecef::Ecef,
+    geometry::{coordinate::Coordinate, ecef::Ecef},
     spatial_id::{SpatialId, helpers::Dimension, single::SingleId},
 };
 pub fn get_length_of_voxel(v: SingleId, a: Dimension) -> f64 {
@@ -11,4 +11,12 @@ pub fn get_length_of_voxel(v: SingleId, a: Dimension) -> f64 {
             r * 2.0 * std::f64::consts::PI / (2_i32.pow(v.as_z() as u32) as f64)
         }
     }
+}
+pub fn difference(a: Coordinate, b: Coordinate) -> f64 {
+    let ecef_a: Ecef = a.into();
+    let ecef_b: Ecef = b.into();
+    let x = ecef_a.as_x() - ecef_b.as_x();
+    let y = ecef_a.as_y() - ecef_b.as_y();
+    let z = ecef_a.as_z() - ecef_b.as_z();
+    (x * x + y * y + z * z).sqrt()
 }
