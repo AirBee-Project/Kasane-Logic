fe5776d84d44ebd53b798f48c0fcbade77e1de5c
Tomoro Saito <hg1190159@hiroogakuen.com>
2026-01-09 17:57:32 +0900

    Segmentのエンコードとデコードを作成

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,64 @@
-use kasane_logic::spatial_id::segment::{Segment, encode::EncodeSegment};
+use std::collections::HashSet;
+
+use kasane_logic::spatial_id::{
+    constants::{F_MAX, F_MIN},
+    segment::{self, Segment, encode::EncodeSegment},
+};
+use rand::Rng;
 
 fn main() {
-    let test: Vec<Segment<i64>> = Segment::<i64>::new(2, [-1, -1]).collect();
+    let segment: Vec<_> = Segment::<i32>::new(1, [-2, 1]).collect();
 
-    let first = test.first().unwrap();
-
-    let encode = EncodeSegment::from(first.clone());
-
-    println!("{:?}", first);
-    println!("{}", encode);
-    // println!("{:?}", Segment::<i64>::from(encode))
+    println!("{:?}", segment);
 }
+
+// fn main() {
+//     let mut rng = rand::rng();
+//     #[derive(PartialEq, Eq, Hash)]
+//     struct Kiroku {
+//         z: u8,
+//         dimension: [i32; 2],
+//     }
+//     let mut set = HashSet::new();
+
+//     for i in 0..1000000 {
+//         let z = rng.random_range(0..20) as usize;
+//         let mut dimension = [
+//             rng.random_range(F_MIN[z]..=F_MAX[z]),
+//             rng.random_range(F_MIN[z]..=F_MAX[z]),
+//         ];
+//         // println!("z={}", z);
+//         // println!("{:?}", dimension);
+
+//         if dimension[0] > dimension[1] {
+//             dimension.swap(0, 1);
+//         }
+
+//         let segment: Vec<_> = Segment::<i32>::new(z as u8, dimension).collect();
+
+//         if segment.first().is_none() {
+//             set.insert(Kiroku {
+//                 z: z as u8,
+//                 dimension,
+//             });
+//         }
+
+//         // let first = segment.first().unwrap().clone();
+
+//         // let encode = EncodeSegment::from(first);
+
+//         // let decode = Segment::<i32>::from(encode.clone());
+
+//         // if first != decode {
+//         //     println!("{:?}", first);
+//         //     println!("{:?}", encode);
+//         //     println!("{:?}", decode);
+
+//         //     panic!()
+//         // }
+//     }
+
+//     for ele in set {
+//         println!("Z={} F={:?},", ele.z, ele.dimension);
+//     }
+// }
