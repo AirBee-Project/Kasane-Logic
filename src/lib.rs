a11cc2c0541f03f37d2f45c11f9051fb8ce74a76
Tomoro Saito <hg1190159@hiroogakuen.com>
2026-01-19 15:46:51 +0900

    型、Trait、関数の見え方を整理

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
-use std::ops::{BitAnd, BitOr, Deref, DerefMut, Not, Sub};
-
-use crate::spatial_id::{
-    SpatialIdEncode,
-    collection::{
-        map::{MapLogic, OnMemoryMap},
-        set::OnMemorySet,
-    },
-};
-
 /// 発生し得るすべてのエラーを`enum` 型として定義・集約。
 mod error;
 
@@ -18,65 +8,16 @@ mod geometry;
 mod spatial_id;
 
 /// このライブライがサポートするストレージのTrait定義と実装
-pub mod kv;
+mod kv;
+
+pub use roaring::RoaringTreemap;
 
 pub use error::Error;
-pub use geometry::{coordinate::Coordinate, ecef::Ecef};
-pub use spatial_id::{range_id::RangeId, single_id::SingleId};
-
-#[derive(Clone)]
-pub struct SpatialIdMap<V>(MapLogic<OnMemoryMap<V>>);
-impl<V> SpatialIdMap<V>
-where
-    V: Clone + PartialEq,
-{
-    pub fn new() -> Self {
-        Self(MapLogic::new(OnMemoryMap::default()))
-    }
-}
-
-impl<V> Default for SpatialIdMap<V>
-where
-    V: Clone + PartialEq,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<V> Deref for SpatialIdMap<V> {
-    type Target = MapLogic<OnMemoryMap<V>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<V> DerefMut for SpatialIdMap<V> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-#[derive(Clone, Default)]
-pub struct SpatialIdSet(OnMemorySet);
-
-impl SpatialIdSet {
-    pub fn new() -> Self {
-        Self(OnMemorySet::new())
-    }
-}
-
-impl Deref for SpatialIdSet {
-    type Target = OnMemorySet;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for SpatialIdSet {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
+pub use geometry::{
+    coordinate::Coordinate, ecef::Ecef, shapes::line, shapes::sphere, shapes::triangle,
+};
+pub use kv::KvStore;
+pub use spatial_id::{
+    collection::MapIndexTrait, collection::MapTrait, collection::map::MapLogic,
+    collection::set::SetLogic, range_id::RangeId, single_id::SingleId,
+};
