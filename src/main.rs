6c27092ce23f302a9186b38eb868edfff05ad052
Tomoro Saito <hg1190159@hiroogakuen.com>
2026-03-06 11:38:29 +0900

    SpatialIdSetのTraitを作成

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-use kasane_logic::{RangeId, SpatialId};
-
-fn main() {
-    let id1 = RangeId::random_at(10);
-    println!("{}", id1);
-    let all = id1.single_ids().into_iter().count();
-    let optimze = id1.optimize_single_ids().into_iter().count();
-
-    println!("{}>{}", all, optimze)
-}
+fn main() {}
