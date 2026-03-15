9c27b119479b21281f2c650da3035d1d28c2d85f
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-03-15 12:50:37 +0900

    依存関係を整理

--- src/spatial_id/traits.rs
+++ src/spatial_id/traits.rs
@@ -1,6 +1,4 @@
-use crate::{
-    Coordinate, FlexId, Segment, SingleId, error::Error, spatial_id::temporal_id::TemporalId,
-};
+use crate::{Coordinate, SingleId, error::Error, spatial_id::temporal_id::TemporalId};
 
 /// 空間 ID が備えるべき基礎的な性質および移動操作を定義するトレイト。
 pub trait SpatialId {
