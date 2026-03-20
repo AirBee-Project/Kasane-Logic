4ba258caeaf71c75fd409b3a78957c29cfe17483
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-03-20 16:27:50 +0900

    三角形のベンチマークを追加

--- benches/triangle.rs
+++ benches/triangle.rs
@@ -1,33 +1,27 @@
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
-use kasane_logic::{Coordinate, Geometry, Triangle};
 use std::hint::black_box;
 
-/// ズームレベルによる負荷の変化を計測
-fn big_triangle_z_bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Triangle ZoomLeveL Scaling");
+use kasane_logic::{Coordinate, Geometry, Triangle};
+use memori::{Bench, Func};
 
-    //Z= 15-25までをテスト
-    let inputs = 15..=25;
+fn main() {
+    let mut func = Func::new("Triangle関数の可視化")
+        .add_bench(
+            "Z",
+            "Zの変化によるスケーリング",
+            Bench::Scaling(vec![18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]),
+        )
+        .add_function("通常の三角形関数", |z| {
+            let tokyo_station = Coordinate::new(35.681000, 139.767000, 0.0).unwrap();
+            let point_b = Coordinate::new(35.681200, 139.767200, 10.0).unwrap();
+            let point_c = Coordinate::new(35.680700, 139.767050, 5.0).unwrap();
 
-    // 東京駅 八重洲口付近
-    let tokyo_station = Coordinate::new(35.681000, 139.767000, 0.0).unwrap();
-    let point_b = Coordinate::new(35.681200, 139.767200, 10.0).unwrap();
-    let point_c = Coordinate::new(35.680700, 139.767050, 5.0).unwrap();
+            let triangle = Triangle::new([tokyo_station, point_b, point_c]);
 
-    let triangle = Triangle::new([tokyo_station, point_b, point_c]);
-
-    for z in inputs {
-        group.bench_with_input(BenchmarkId::new("Z", z), &z, |b, &z| {
-            b.iter(|| {
-                let iter = triangle
-                    .single_ids(z as u8)
-                    .expect("Failed to get iterator");
-                black_box(iter.count())
-            });
+            let iter = triangle
+                .single_ids(*z as u8)
+                .expect("Failed to get iterator");
+            black_box(iter.count())
         });
-    }
-    group.finish();
-}
 
-criterion_group!(benches, big_triangle_z_bench);
-criterion_main!(benches);
+    func.run_and_save().unwrap();
+}
