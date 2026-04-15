3e49a4e2ee19b7cca9607d9e73f3ec81731a3c65
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-04-15 18:35:45 +0900

    全てをlib.rsから公開

--- src/lib.rs
+++ src/lib.rs
@@ -8,51 +8,31 @@ pub mod spatial_id;
 
 pub use error::Error;
 
-/// よく使うトレイト群をまとめた prelude。
-pub mod prelude {
-    pub use crate::geometry::point::traits::Point;
-    pub use crate::geometry::shapes::traits::{
-        IntoCoordinates, IntoLines, IntoPolygons, IntoTriangles, Shape,
-    };
-    pub use crate::spatial_id::collection::traits::{SpatialIdSet, SpatialIdTable};
-    pub use crate::spatial_id::traits::{
-        IntoFlexIds, IntoSingleIds, IterFlexIds, IterSingleIds, SpatialId,
-    };
-}
-
-// クレート内部実装は既存の `crate::TypeName` 参照を維持する。
-#[doc(hidden)]
+// geometry
 pub use geometry::constants::{WGS84_A, WGS84_B, WGS84_E2, WGS84_F, WGS84_INV_F};
-#[doc(hidden)]
-pub use geometry::point::{coordinate::Coordinate, ecef::Ecef, traits::Point};
-#[doc(hidden)]
-pub use geometry::shapes::{
-    line::Line,
-    polygon::Polygon,
-    solid::Solid,
-    sphere::Sphere,
-    traits::{IntoCoordinates, IntoLines, IntoPolygons, IntoTriangles, Shape},
-    triangle::Triangle,
+pub use geometry::point::coordinate::Coordinate;
+pub use geometry::point::ecef::Ecef;
+pub use geometry::point::traits::Point;
+pub use geometry::shapes::line::Line;
+pub use geometry::shapes::polygon::Polygon;
+pub use geometry::shapes::solid::Solid;
+pub use geometry::shapes::sphere::Sphere;
+pub use geometry::shapes::traits::{
+    IntoCoordinates, IntoLines, IntoPolygons, IntoTriangles, Shape,
 };
-#[doc(hidden)]
-pub use spatial_id::collection::flex_tree::{
-    core::FlexTreeCore, map::FlexTreeMap, set::FlexTreeSet,
-};
-#[doc(hidden)]
+pub use geometry::shapes::triangle::Triangle;
+pub use geometry::traits::{ToFlexIds, ToRangeIds, ToSingleIds};
+
+// spatial_id
+pub use spatial_id::collection::flex_tree::core::FlexTreeCore;
+pub use spatial_id::collection::flex_tree::map::FlexTreeMap;
+pub use spatial_id::collection::flex_tree::set::FlexTreeSet;
 pub use spatial_id::collection::traits::{SpatialIdSet, SpatialIdTable};
-#[doc(hidden)]
 pub use spatial_id::constants::{F_MAX, F_MIN, MAX_ZOOM_LEVEL, XY_MAX};
-#[doc(hidden)]
 pub use spatial_id::flex_id::FlexId;
-#[doc(hidden)]
 pub use spatial_id::flex_id::segment::Segment;
-#[doc(hidden)]
 pub use spatial_id::helpers::{Dimension, Side};
-#[doc(hidden)]
 pub use spatial_id::range_id::RangeId;
-#[doc(hidden)]
 pub use spatial_id::single_id::SingleId;
-#[doc(hidden)]
 pub use spatial_id::temporal_id::TemporalId;
-#[doc(hidden)]
 pub use spatial_id::traits::{IntoFlexIds, IntoSingleIds, IterFlexIds, IterSingleIds, SpatialId};
