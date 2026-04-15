22a3b80d12e48cb9a872148b4fada4bc656fec60
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-04-15 18:41:48 +0900

    APIのルールを追加

--- src/lib.rs
+++ src/lib.rs
@@ -6,33 +6,67 @@ pub mod geometry;
 /// 空間IDに関する型を定義。
 pub mod spatial_id;
 
+#[doc(inline)]
 pub use error::Error;
 
-// geometry
-pub use geometry::constants::{WGS84_A, WGS84_B, WGS84_E2, WGS84_F, WGS84_INV_F};
+// geometry: types
+#[doc(inline)]
 pub use geometry::point::coordinate::Coordinate;
+#[doc(inline)]
 pub use geometry::point::ecef::Ecef;
-pub use geometry::point::traits::Point;
+#[doc(inline)]
 pub use geometry::shapes::line::Line;
+#[doc(inline)]
 pub use geometry::shapes::polygon::Polygon;
+#[doc(inline)]
 pub use geometry::shapes::solid::Solid;
+#[doc(inline)]
 pub use geometry::shapes::sphere::Sphere;
+#[doc(inline)]
+pub use geometry::shapes::triangle::Triangle;
+
+// geometry: traits
+#[doc(inline)]
+pub use geometry::point::traits::Point;
+#[doc(inline)]
 pub use geometry::shapes::traits::{
     IntoCoordinates, IntoLines, IntoPolygons, IntoTriangles, Shape,
 };
-pub use geometry::shapes::triangle::Triangle;
+#[doc(inline)]
 pub use geometry::traits::{ToFlexIds, ToRangeIds, ToSingleIds};
 
-// spatial_id
-pub use spatial_id::collection::flex_tree::core::FlexTreeCore;
-pub use spatial_id::collection::flex_tree::map::FlexTreeMap;
-pub use spatial_id::collection::flex_tree::set::FlexTreeSet;
-pub use spatial_id::collection::traits::{SpatialIdSet, SpatialIdTable};
-pub use spatial_id::constants::{F_MAX, F_MIN, MAX_ZOOM_LEVEL, XY_MAX};
+// geometry: constants
+#[doc(inline)]
+pub use geometry::constants::{WGS84_A, WGS84_B, WGS84_E2, WGS84_F, WGS84_INV_F};
+
+// spatial_id: id types
+#[doc(inline)]
 pub use spatial_id::flex_id::FlexId;
+#[doc(inline)]
 pub use spatial_id::flex_id::segment::Segment;
-pub use spatial_id::helpers::{Dimension, Side};
+#[doc(inline)]
 pub use spatial_id::range_id::RangeId;
+#[doc(inline)]
 pub use spatial_id::single_id::SingleId;
+#[doc(inline)]
 pub use spatial_id::temporal_id::TemporalId;
+
+// spatial_id: collection types
+#[doc(inline)]
+pub use spatial_id::collection::flex_tree::core::FlexTreeCore;
+#[doc(inline)]
+pub use spatial_id::collection::flex_tree::map::FlexTreeMap;
+#[doc(inline)]
+pub use spatial_id::collection::flex_tree::set::FlexTreeSet;
+
+// spatial_id: traits
+#[doc(inline)]
+pub use spatial_id::collection::traits::{SpatialIdSet, SpatialIdTable};
+#[doc(inline)]
+pub use spatial_id::helpers::{Dimension, Side};
+#[doc(inline)]
 pub use spatial_id::traits::{IntoFlexIds, IntoSingleIds, IterFlexIds, IterSingleIds, SpatialId};
+
+// spatial_id: constants
+#[doc(inline)]
+pub use spatial_id::constants::{F_MAX, F_MIN, MAX_ZOOM_LEVEL, XY_MAX};
