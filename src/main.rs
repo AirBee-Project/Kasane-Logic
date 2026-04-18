fd9729ad21d69c2b5b8c6f97bf6ec245c9702f1b
Tomoro Saito <s.tomoro0726@gmail.com>
2026-04-18 21:32:41 +0900

    mainの不要なコードを削除

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use kasane_logic::{FlexTreeSet, FlexTreeTable, IntoFlexIds, RangeId, SingleId};
+use kasane_logic::{FlexTreeTable, RangeId, SingleId};
 
 fn main() {
     let mut table = FlexTreeTable::new();
 
     let id1 = RangeId::new(5, [-3, 10], [0, 9], [5, 10]).unwrap();
-    let id2 = RangeId::new(4, [3, 6], [2, 2], [1, 9]).unwrap();
-    let id3 = SingleId::new(2, 0, 1, 1).unwrap();
+    // let id2 = RangeId::new(4, [3, 6], [2, 2], [1, 9]).unwrap();
+    // let id3 = SingleId::new(2, 0, 1, 1).unwrap();
 
     table.insert(id1, "Neko".to_string());
 }
