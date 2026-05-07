6b7fb694715a3cddeb2de1911fd6f4fca1f76884
knyg2 <knyg2313@gmail.com>
2026-05-07 22:11:08 +0900

    CoverSingleIdsトレイトを実装

--- src/geometry/shapes/tube/impls.rs
+++ src/geometry/shapes/tube/impls.rs
@@ -1,1 +1,15 @@
+use crate::{CoverSingleIds, Cylinder, Error, SingleId, Sphere, Tube};
+use std::collections::HashSet;
 
+impl CoverSingleIds for Tube {
+    fn cover_single_ids(&self, z: u8) -> Result<impl Iterator<Item = SingleId>, Error> {
+        let mut ids: HashSet<_> = Sphere::new(self.points[0], self.radius_m)?
+            .cover_single_ids(z)?
+            .collect();
+        for coos in self.points.windows(2) {
+            ids.extend(Cylinder::new(coos[0], coos[1], self.radius_m)?.cover_single_ids(z)?);
+            ids.extend(Sphere::new(coos[1], self.radius_m)?.cover_single_ids(z)?);
+        }
+        Ok(ids.into_iter())
+    }
+}
