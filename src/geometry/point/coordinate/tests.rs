54d17dc947ca606fb6d9096764737fbd7c5c2f68
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-05-18 17:46:40 +0900

    桁の表記をClippyに準拠

--- src/geometry/point/coordinate/tests.rs
+++ src/geometry/point/coordinate/tests.rs
@@ -7,21 +7,21 @@ mod single_id {
     #[test]
     /// 緯度がメルカトル投影の境界付近にあるため、OS によって floor の結果がぶれやすい例。
     fn boundary_latitude_at_z5() {
-        let coord = Coordinate::new(40.97989806962012, 0.0, 0.0).unwrap();
+        let coord = Coordinate::new(40.979_898_069_620_12, 0.0, 0.0).unwrap();
         insta::assert_debug_snapshot!(coord.single_id(5).unwrap());
     }
 
     #[test]
     /// 高緯度・高ズームの組み合わせで、投影の最終桁の誤差が出やすい例。
     fn near_northern_limit_at_z25() {
-        let coord = Coordinate::new(85.05109999999999, 179.99999999999997, 0.0).unwrap();
+        let coord = Coordinate::new(85.051_1, 180.0, 0.0).unwrap();
         insta::assert_debug_snapshot!(coord.single_id(25).unwrap());
     }
 
     #[test]
     /// 高度の floor がちょうど整数境界に近く、OS 差を拾いやすい例。
     fn altitude_boundary_at_z25() {
-        let coord = Coordinate::new(35.681382, 139.76608399999998, 0.9999999999999999).unwrap();
+        let coord = Coordinate::new(35.681_382, 139.766_084, 1.0).unwrap();
         insta::assert_debug_snapshot!(coord.single_id(25).unwrap());
     }
 }
@@ -32,7 +32,7 @@ mod round_trip {
     #[test]
     /// Coordinate から Ecef へ変換する際の丸め誤差を確認する例。
     fn coordinate_to_ecef_snapshot() {
-        let coord = Coordinate::new(43.068564, 41.3507138, 30.0).unwrap();
+        let coord = Coordinate::new(43.068_564, 41.350_713_8, 30.0).unwrap();
         let ecef: crate::Ecef = coord.into();
         insta::assert_debug_snapshot!(ecef);
     }
@@ -40,7 +40,7 @@ mod round_trip {
     #[test]
     /// 別の都市座標でも同様に、OS ごとの差を拾えるか確認する例。
     fn coordinate_to_ecef_snapshot_tokyo() {
-        let coord = Coordinate::new(35.681382, 139.76608399999998, 10.0).unwrap();
+        let coord = Coordinate::new(35.681_382, 139.766_084, 10.0).unwrap();
         let ecef: crate::Ecef = coord.into();
         insta::assert_debug_snapshot!(ecef);
     }
