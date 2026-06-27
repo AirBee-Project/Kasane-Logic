a090b31ac674ec2e68f1364a3842bf706b690358
Tomoro Saito <s.tomoro0726@gmail.com>
2026-06-27 15:06:41 +0900

    集合演算についていくつかDocsを追加

--- src/spatial_id/collection/flex_tree/set/mod.rs
+++ src/spatial_id/collection/flex_tree/set/mod.rs
@@ -89,42 +89,41 @@ impl SpatialIdSet {
         self.inner.insert(target, ());
     }
 
+    /// 集合から指定した空間IDと重なる空間IDを切り出して返す。
     pub fn get<'a, S>(&'a self, target: &'a S) -> impl Iterator<Item = FlexId> + 'a
     where
         S: IterFlexIds,
     {
         self.inner.get(target).map(move |(flex_id, _value)| flex_id)
     }
-
-    pub fn remove<S: IterFlexIds>(&mut self, target: &S) -> impl Iterator<Item = FlexId> {
+    /// 集合から指定した空間IDと重なる空間IDを切り出して削除する。
+    /// 削除した部分の空間IDを返す。
+    pub fn remove<S: SpatialId>(&mut self, target: &S) -> impl Iterator<Item = FlexId> {
         self.inner
             .remove(target)
             .map(move |(flex_id, _value)| flex_id)
     }
 
-    /// [`get`](Self::get) と異なり切り取りを行わず、target と重なった
-    /// [`FlexId`] をそのままの返します。
+    /// 指定した空間IDと接触していたすべての空間IDを返す。
+    /// [`get`](Self::get) と異なり切り取りを行わず、target と重なった [`FlexId`] をそのままの返す。
     pub fn get_overlapping<'a, S>(&'a self, target: &'a S) -> impl Iterator<Item = FlexId> + 'a
     where
-        S: IterFlexIds + 'a,
+        S: SpatialId + 'a,
     {
         self.inner
             .get_overlapping(target)
             .map(|(flex_id, _value)| flex_id)
     }
 
-    /// [`get`](Self::get) と異なり切り取りを行わず、target と重なった
-    /// [`FlexId`] をそのままの返します。
-    pub fn remove_overlapping<S: IterFlexIds>(
-        &mut self,
-        target: &S,
-    ) -> impl Iterator<Item = FlexId> {
+    /// 指定した空間IDと接触していたすべての空間IDを削除する。削除した空間IDを返す。
+    /// [`remove`](Self::remove) と異なり切り取りを行わず、target と重なった [`FlexId`] をそのまま返す。
+    pub fn remove_overlapping<S: SpatialId>(&mut self, target: &S) -> impl Iterator<Item = FlexId> {
         self.inner
             .remove_overlapping(target)
             .map(move |(flex_id, _value)| flex_id)
     }
 
-    /// 指定した単体の空間 IDと面で接している[`FlexId`] を重複なく返します。入力された空間ID自身と重なる要素は除外します。
+    /// 指定した単体の空間 IDと面で接している[`FlexId`] を重複なく返す。入力された空間ID自身と重なる空間IDは除外する。
     pub fn neighbors_share_face<S: SpatialId>(
         &self,
         target: &S,
@@ -134,20 +133,24 @@ impl SpatialIdSet {
             .map(|(flex_id, _value)| flex_id)
     }
 
+    /// 集合の内部にある[FlexId]の個数を返す。
     pub fn count(&self) -> usize {
         self.inner.count()
     }
 
+    /// 集合の内部にある全ての[FlexId]のうち、最大のズームレベル値を返す。
     pub fn max_zoomlevel(&self) -> Option<u8> {
         self.inner.max_zoomlevel()
     }
 
+    /// [SpatialIdSet]の最大のズームレベル値に揃えて、すべてを `SingleId` として返す。
     pub fn flat_single_ids(&self) -> impl Iterator<Item = SingleId> {
         self.inner
             .flat_single_ids_ref()
             .map(|(single_id, _)| single_id)
     }
 
+    /// [SpatialIdSet]の内部の空間IDを全て削除します。
     pub fn clear(&mut self) {
         self.inner.clear();
     }
@@ -157,6 +160,7 @@ impl SpatialIdSet {
         self.inner.root_ptr_eq(&other.inner)
     }
 
+    /// [SpatialIdSet]の内部が空かどうかを判定します。
     pub fn is_empty(&self) -> bool {
         self.inner.is_empty()
     }
