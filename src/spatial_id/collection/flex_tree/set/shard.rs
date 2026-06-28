e2de9b498483afd549fb33d34959c1d8e2d8888a
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-06-29 02:07:10 +0900

    シャード領域を取得できるAPIを追加

--- src/spatial_id/collection/flex_tree/set/shard.rs
+++ src/spatial_id/collection/flex_tree/set/shard.rs
@@ -1,6 +1,13 @@
+use crate::FlexId;
+
 use super::SpatialIdSet;
 
 impl SpatialIdSet {
+    /// シャード領域を返す。`None` が帰ってきた場合はシャードされていない。
+    pub fn shard(&self) -> Option<&FlexId> {
+        self.inner.shard()
+    }
+
     pub fn should_split_shard(&self, max_flex_id_count: usize) -> bool {
         self.inner.should_split_shard(max_flex_id_count)
     }
