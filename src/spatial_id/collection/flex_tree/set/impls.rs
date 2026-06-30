6c7584815bc99957e1ffdcb7c444fb6d83df11c3
Tomoro Saito <s.tomoro0726@gmail.com>
2026-07-01 02:35:57 +0900

    集合でやみくもにIterFlexIdsを使わないように改良

--- src/spatial_id/collection/flex_tree/set/impls.rs
+++ src/spatial_id/collection/flex_tree/set/impls.rs
@@ -3,8 +3,10 @@ use crate::{IterFlexIds, SpatialIdSet};
 impl<S: IterFlexIds> FromIterator<S> for SpatialIdSet {
     fn from_iter<T: IntoIterator<Item = S>>(iter: T) -> Self {
         let mut set = SpatialIdSet::new();
-        for item in iter {
-            set.insert(item);
+        for items in iter {
+            for item in items.iter_flex_ids() {
+                set.insert(item);
+            }
         }
         set
     }
@@ -12,8 +14,10 @@ impl<S: IterFlexIds> FromIterator<S> for SpatialIdSet {
 
 impl<S: IterFlexIds> Extend<S> for SpatialIdSet {
     fn extend<T: IntoIterator<Item = S>>(&mut self, iter: T) {
-        for item in iter {
-            self.insert(item);
+        for items in iter {
+            for item in items.iter_flex_ids() {
+                self.insert(item);
+            }
         }
     }
 }
