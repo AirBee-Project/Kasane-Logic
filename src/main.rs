b60a47fc8282edde7ad3abffcfd50aec9eab87af
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-07-26 12:11:32 +0900

    コメントの修正

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ fn main() {
     let bldg_risk: SpatialIdTable<u32> =
         serde_json::from_str(&fs::read_to_string("sample/bldg_risk.json").unwrap()).unwrap();
 
-    println!("{}", bldg_risk.bounding_box().unwrap());
-
-    let risk: kasane_logic::SpatialIdTable<u32> = bldg_risk
+    let risk = bldg_risk
         .query()
         .zoom_out(22, Average)
         .falloff_linear_x(25, 3, Max)
         .falloff_linear_y(25, 3, Max)
-        .run_table()
+        .raw_run_table()
         .unwrap();
 
     let json_string = serde_json::to_string(&risk).unwrap();
--- src/spatial_id/collection/query/mod.rs
+++ src/spatial_id/collection/query/mod.rs
@@ -4,13 +4,13 @@ pub mod ops;
 /// 演算定義のTrait
 pub mod traits;
 
-/// 式全体を見て、最適化し、実行するためのモジュール（領域限定の遅延評価を含む）
+/// 式全体を見て、最適化し、実行するためのモジュール
 pub mod execution;
 
 /// 複数の値が同じ空間で衝突した際の解決ポリシー
 pub mod merge_policy;
 
-/// クエリの入力源（`Source`）
+/// クエリの実行用Trait
 pub mod source;
 
 /// クエリの表示の実装
