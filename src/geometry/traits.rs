76a4bd0b0b96ee494305fdba499d222f6c92d52c
Tomoro0726 <s.tomoro0726@gmail.com>
2026-08-27 09:52:52 +0900

    不要なTraitを削除しDocのエラーを解消

--- src/geometry/traits.rs
+++ src/geometry/traits.rs
@@ -16,10 +16,10 @@ pub trait CoverSingleIds {
 
 pub trait CoverRangeIds {
     type Value;
-    /// 指定されたズームレベルの[RangeId]を出力する。
+    /// 指定されたズームレベルの[crate::RangeId]を出力する。
     ///
-    /// [CoverSingleIds] の結果を単純に [RangeId] へ変換するラッパーではなく、
-    /// 実装内部で [RangeId] の出力を活かす処理を持つこと。
+    /// [CoverSingleIds] の結果を単純に [crate::RangeId] へ変換するラッパーではなく、
+    /// 実装内部で [crate::RangeId] の出力を活かす処理を持つこと。
     fn cover_range_ids_with(
         &self,
         z: u8,
@@ -52,18 +52,3 @@ impl<T: CoverRangeIds, V: Clone> CoverRangeIds for (T, V) {
     }
 }
 
-pub trait CoverMapExt {
-    type Shape;
-    type Value;
-
-    /// 図形を分解する前に、付随する値を事前に変換する
-    fn map_value<U, F: FnOnce(Self::Value) -> U>(self, f: F) -> (Self::Shape, U);
-}
-
-impl<T, V> CoverMapExt for (T, V) {
-    type Shape = T;
-    type Value = V;
-    fn map_value<U, F: FnOnce(Self::Value) -> U>(self, f: F) -> (Self::Shape, U) {
-        (self.0, f(self.1))
-    }
-}
